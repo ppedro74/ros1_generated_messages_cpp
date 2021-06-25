b8896b2e9f5a0351fcab47e4aa53aba77180e87f
ptp <pedro.pereira@cignon.com>
2021-06-24 23:38:35 -0400

    added tf msgs added dynamic_reconfigure msgs added realsense2_camera msgs

--- dummy.cpp
+++ dummy.cpp
@@ -2,12 +2,15 @@
 #include "std_msgs/Time.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "tf2_msgs/TFMessage.h"
+#include "tf/tfMessage.h"
 //#include "hector_nav_msgs/GetDistanceToObstacleResponse.h"
 
 void dummy()
 {
 	std_msgs::Time time_msg;
 	nav_msgs::OccupancyGrid occupancy_grid;
-	tf2_msgs::TFMessage tf_message;
+	tf2_msgs::TFMessage tf_message2;
+	tf::tfMessage tf_message;
+
 	//hector_nav_msgs::GetDistanceToObstacleResponse get_distance_to_obstacle_response;
 }
